18dce1c7de622e729bb0413009b85d4c4f936471
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-12-23 11:23:04 -0500

    DAY6

---
+++ Day6/SearchInRotatedSortedArray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums, target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        while l<=r:
+            mid = int((l+r)/2)
+            if nums[mid]==target:
+                return mid
+            if nums[l]<=nums[mid]:
+                if target>nums[mid] or target<nums[l]:
+                    l=mid+1
+                else:
+                    r=mid-1
+            else:
+                if target<nums[mid] or target>nums[r]:
+                    r=mid-1
+                else:
+                    l=mid+1
+
+        return -1
