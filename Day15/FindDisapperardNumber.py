d3e3c4e748d019d38793b7ae31c171c29e432107
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-12-26 16:03:54 -0500

    DAY15

--- Day15/FindDisapperardNumber.py
+++ Day15/FindDisapperardNumber.py
@@ -1,11 +1,21 @@
+# BRUTE FORCE
+# class Solution:
+#     def findDisappearedNumbers(self, nums):
+#         res =[]
+#         hasmap={}
+#         for x in nums:
+#             hasmap[x]=True
+#         for y in range(1,len(nums)+1):
+#             if y not in hasmap:
+#                 res.append(y)
+#         return res
 class Solution:
-    def findDisappearedNumbers(self, nums):
-        res =[]
-        hasmap={}
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         for x in nums:
-            hasmap[x]=True
-        for y in range(1,len(nums)+1):
-            if y not in hasmap:
-                res.append(y)
+            i=abs(x)-1
+            nums[i]=-1*abs(nums[i])
+        res = []
+        for y in range(len(nums)):
+            if nums[y]>0:
+                res.append(y+1)
         return res
-        
