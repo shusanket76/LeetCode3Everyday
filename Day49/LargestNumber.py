cd8e466fa04d84d0036306c034bd1dddc915146b
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-01-30 16:24:23 -0500

    DAY49

--- Day49/LargestNumber.py
+++ Day49/LargestNumber.py
@@ -1,11 +1,16 @@
-def largestNumber(nums) -> str:
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
         for y in range(len(nums)):
             for x in range(len(nums)-1-y):
                 a = str(nums[x])+str(nums[x+1])
                 b = str(nums[x+1])+str(nums[x])
                 if int(a)<int(b):
                     nums[x],nums[x+1]=nums[x+1], nums[x]
-        print(nums)
+        res = ""
+        for a in nums:
+            res+=str(a)
+        if int(res)==0:
+            return "0"
+        return res
 
-
-a = largestNumber([3,30,34,5,9])
+        
