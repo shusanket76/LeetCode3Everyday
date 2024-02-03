8b803a48b0e1d418bf045f045cdb33b33d0f4c53
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-02-03 03:22:04 -0500

    optimized

--- Day5/FindMinimumInRotatedSortedArray.py
+++ Day5/FindMinimumInRotatedSortedArray.py
@@ -1,23 +1,44 @@
-class Solution:
-    def findMin(self, nums) -> int:
-        l=0
-        r=len(nums)-1
-        ans = nums[0]
-        while l<=r:
-            mid = int((l+r)/2)
-            ans = min(nums[mid], ans)
-            # first group
-            if nums[mid]>=nums[l]:
-                if nums[r]<nums[l]:
-                    l=mid+1
-                else:
-                    r=mid-1
-            # second group
-            else:
-                if r-mid>1:
-                    r=mid-1
-                else:
-                    l=mid+1
-        return ans
+# class Solution:
+#     def findMin(self, nums) -> int:
+#         l=0
+#         r=len(nums)-1
+#         ans = nums[0]
+#         while l<=r:
+#             mid = int((l+r)/2)
+#             ans = min(nums[mid], ans)
+#             # first group
+#             if nums[mid]>=nums[l]:
+#                 if nums[r]<nums[l]:
+#                     l=mid+1
+#                 else:
+#                     r=mid-1
+#             # second group
+#             else:
+#                 if r-mid>1:
+#                     r=mid-1
+#                 else:
+#                     l=mid+1
+#         return ans
 
+        
+
+
+# ======================================================================
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums)-1
+        res = nums[l]
+        while l<=r:
+            if nums[l]<=nums[r]:
+                res = min(res, nums[l])
+                return res
+            mid = (l+r)//2
+            res = min(res, nums[mid])
+  
+            if nums[l]<=nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return res
         
--- p2.py
+++ p2.py
@@ -1,11 +1,25 @@
-def largestNumber(nums) -> str:
-        for y in range(len(nums)):
-            for x in range(len(nums)-1-y):
-                a = str(nums[x])+str(nums[x+1])
-                b = str(nums[x+1])+str(nums[x])
-                if int(a)<int(b):
-                    nums[x],nums[x+1]=nums[x+1], nums[x]
-        print(nums)
+def numSubseq(nums, target: int) -> int:
+        nums.sort()
+        res = [0]
 
 
-a = largestNumber([3,30,34,5,9])
+        def dfs(curr, pointer):
+            if pointer>=len(nums) and len(curr)!=0:
+                if curr[0]+curr[-1]<=target:
+                    res[0]+=1
+                return 
+            if pointer>=len(nums):
+                return
+            curr.append(nums[pointer])
+            dfs(curr, pointer+1)
+            curr.pop()
+            dfs(curr, pointer+1)
+
+        
+        curr = []
+        pointer = 0
+        dfs(curr, pointer)
+        print(res)
+nums = [2,3,3,4,6,7]
+target = 12
+a = numSubseq(nums, target)        
