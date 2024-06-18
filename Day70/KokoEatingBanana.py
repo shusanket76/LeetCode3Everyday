9da32558d40c021a2323aa8e64375029f752e790
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-06-18 15:59:22 -0400

    Day70

--- Day70/KokoEatingBanana.py
+++ Day70/KokoEatingBanana.py
@@ -1,19 +1,17 @@
 import math
-# print(math.ceil(3/3))
-def minEatingSpeed(piles, h: int) -> int:
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l = 1
         r = max(piles)
+        res = r
         while l<=r:
             mid = int(l+r)//2
             temp = 0
             for x in piles:
                 temp+=math.ceil(x/mid)
             if temp>h:
-                r=mid-1
-            else:
                 l=mid+1
-        return r
-
-
-a = minEatingSpeed(piles = [3,6,7,11], h = 8)
-
+            elif temp<=h:
+                res = min(res, mid)
+                r=mid-1
+        return res
