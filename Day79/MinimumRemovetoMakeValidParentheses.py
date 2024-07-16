61f56b0c77b1db757a7ba19518f0a91ea96cce07
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-07-16 18:08:24 -0400

    Day79

---
+++ Day79/MinimumRemovetoMakeValidParentheses.py
@@ -0,0 +1,23 @@
+from collections import deque
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        haset = []
+        closehaset = []
+        stack = deque()
+        newstr = ""
+        for x in range(len(s)):
+            if s[x]=="(":
+                haset.append(x)
+            elif s[x]==")":
+                if len(haset)!=0:
+                    haset.pop()
+                else:
+                    closehaset.append(x)
+        for x in range(len(s)):
+            if x in haset or x in closehaset:
+                continue
+            newstr+=s[x]
+
+        return (newstr)
+
+        
