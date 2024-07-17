a432201c59d61d53b1f5fb1f484509a7134b27ad
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-07-17 10:50:01 -0400

    DAY80

--- Day80/MagneticForceBetweenTwoBalls.py
+++ Day80/MagneticForceBetweenTwoBalls.py
@@ -1,20 +1,25 @@
-def maxDistance(position, m: int) -> int:
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
         position.sort()
 
-        start = position[0]
+        start = 1
         end = position[-1]
+        res = float("-inf")
         while start<=end:
             dis = int((start+end)/2)
             curr = position[0]
-            target = 0
+            target = 1
             for x in range(1,len(position)):
                 if abs(curr-position[x])>=dis:
                     target+=1
                     curr=position[x]
                 if target==m:
+                    res = max(res, dis)
                     start=dis+1
                     break
             if target<m:
                 end = dis-1
-        return dis
-a = maxDistance(position = [5,4,3,2,1,1000000000], m = 3)
+
+        return res
+
+        
